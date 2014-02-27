a301a852b44ae88f6e0458f33c0cd9646dc2869e
ludopradel <lpradel@astek.fr>
2014-02-27 15:18:42 +0100

    initial commit

---
+++ watchlist.spec.js
@@ -0,0 +1,19 @@
+var http = require('http');
+
+
+describe("Tests suite", function() {
+  it("should launch server & return statusCode 200", function() {
+    var server = http.createServer(function (request, response) {
+		response.writeHead(200, { 'Content-Type': 'text/plain' });
+		response.end('Hello, world!\n');
+	});
+	
+	server.listen(8080);
+	
+	http.get('http://localhost:8080', function (response) {
+		expect(response.statusCode).toBe(200);
+	});
+	
+	server.close();
+  });
+});
