d5119beebb489e0e9a0dc1254e319a942f47e7a8
ludopradel <lpradel@astek.fr>
2014-03-10 14:47:07 +0100

    add all tests on npm test

--- tests/link.spec.js
+++ tests/link.spec.js
@@ -1,19 +1,23 @@
 var link = require('../src/link');
 
 describe("link tests", function() {
+  
   it("create a new link containing title", function() {
     var newLink = link.create('website', 'url');
     expect(newLink.title).toBe('website');
   });
+  
   it("create a new link containing url", function() {
     var newLink = link.create('website', 'url');
     expect(newLink.url).toBe('url');
   });
+  
   it("could mark link as read", function() {
     var newLink = link.create('website', 'url');
     newLink.markAsRead();
     expect(newLink.isRead()).toBe(true);
   });
+  
   it("could unmark a reading link", function() {
     var newLink = link.create('website', 'url');
     newLink.markAsRead();
